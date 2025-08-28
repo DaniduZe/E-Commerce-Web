4d425348a288053a02db885dbfdec37f73b9193e
Danidu Sennath Kawudupitiya <110765521+DaniduZe@users.noreply.github.com>
2025-08-28 23:40:45 +0530

    Delete Backend directory

--- Backend/routes/homeRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchProduct, fetchProducts, fetchTrendingProduct } from "../controllers/ProductsController.js";
-
-const route = express.Router();
-route.get("/products", fetchProducts);
-route.get("/fetchProduct/:id", fetchProduct);
-route.get("/fetchTrendingProduct", fetchTrendingProduct);
-export default route;
--- Backend/routes/orderRoute.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { completePayment, createOrder, orderView } from "../controllers/orderController.js";
-import { authMiddleware } from "../middlewares/auth.js";
-
-const route = express.Router();
-route.post("/createOrder", authMiddleware, createOrder);
-route.post("/completePayment", authMiddleware, completePayment);
-route.post("/orderView", orderView);
-export default route;
